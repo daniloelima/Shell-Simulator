abf41c35206325d55fbaf51187d326711b504cd1
enzocussuol <enzo.cussuol@edu.ufes.br>
2021-04-02 17:10:42 -0300

    Leitura dos comandos via string (uma unica string por linha).

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+char* novosComandos(){
+    char strAux[100];
+    char* novosComandos;
+
+    fgets(strAux, 100, stdin);
+
+    novosComandos = strdup(strAux);
+    novosComandos[strlen(novosComandos) - 1] = '\0';
+
+    return novosComandos;
+}
 
 int main(){
-    printf("Hello World!\n");
+    while(1){
+        printf("vsh> ");
+
+        char* comandos = novosComandos();
+
+        printf("%s\n", comandos);
+    }
 
     return 0;
 }
